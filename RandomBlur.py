e81d60b564d09ae8ef82e0f07a00988e39300cd9
Jooney <42194130+jhan756k@users.noreply.github.com>
2022-11-09 00:06:41 +0900

    monalisa

--- RandomBlur.py
+++ RandomBlur.py
@@ -1,12 +1,12 @@
 import cv2
 import random
-image = cv2.imread("./images/html.png")
+image = cv2.imread("./images/monalisa.png")
 
-blur_pixel = 1000
+blur_pixel = 3000
 
 for _ in range(blur_pixel):
     x = random.randint(0, image.shape[0] - 1)
     y = random.randint(0, image.shape[1] - 1)
     image[x, y] = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
-cv2.imwrite("./images/html_blur.png", image)
+cv2.imwrite("./images/monalisa_blur.png", image)
