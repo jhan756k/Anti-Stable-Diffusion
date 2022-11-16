32446d9f76a19588bf0ec872b2b66619efe3196a
Jinho Kim <79720217+real-lake050317@users.noreply.github.com>
2022-11-16 23:38:18 +0900

    .

--- RandomBlur.py
+++ RandomBlur.py
@@ -1,11 +1,11 @@
 import cv2, random
-image = cv2.imread("./images/monalisa.png")
+image = cv2.imread("./images/egon.jpg")
 
-blur_rate = 0.25
+blur_rate = 1
 
 for _ in range(int((image.shape[0]*image.shape[1])*blur_rate)):
     x = random.randint(0, image.shape[0] - 1)
     y = random.randint(0, image.shape[1] - 1)
     image[x, y] = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
-cv2.imwrite("./images/monalisa_blur.png", image)
+cv2.imwrite("./images/egon_noise.jpg", image)
