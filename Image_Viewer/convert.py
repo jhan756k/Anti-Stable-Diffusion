7bf8359471e2878ab479754468264ee981a8eb26
Jooney <42194130+jhan756k@users.noreply.github.com>
2022-11-19 20:40:18 +0900

    bytecompress

--- Image_Viewer/convert.py
+++ Image_Viewer/convert.py
@@ -1,15 +1,17 @@
-import os, cv2
+import os, cv2, zlib
 
-file = open("./gorani.jh", "w")
+file = open("./gorani.jh", "wb")
 
 path = os.getcwd()
 img = cv2.imread("{p}/images/original_image/gorani.png".format(p=path))
 
-file.write(str(img.shape[0]).zfill(3) + str(img.shape[1]).zfill(3) + str(img.shape[2]).zfill(3))
+text = str(img.shape[0]).zfill(3) + str(img.shape[1]).zfill(3) + str(img.shape[2]).zfill(3)
 
 for x in range(img.shape[0]):
     for y in range(img.shape[1]):
-        file.write(str(img[x, y][0]).zfill(3) + str(img[x, y][1]).zfill(3) + str(img[x, y][2]).zfill(3))
-    file.write(" ")
+        text += (str(img[x, y][0]).zfill(3) + str(img[x, y][1]).zfill(3) + str(img[x, y][2]).zfill(3))
+    text += " "
 
+comp_text = zlib.compress(text.encode("utf-8"))
+file.write(comp_text)
 file.close()
--- Image_Viewer/read.py
+++ Image_Viewer/read.py
@@ -1,17 +1,18 @@
 import numpy as np
-import cv2
+import cv2, zlib
 
-file = open("gorani.jh", "r")
+file = open("gorani.jh", "rb")
 content = file.read()
+content = zlib.decompress(content).decode("utf-8")
+
 stmp = content[:9]
 size = [int(stmp[:3]), int(stmp[3:6]), int(stmp[6:9])]
-content = content[9:]
 content = list(content.split(" "))
 
 img = np.zeros((size[0], size[1], size[2]), dtype=np.uint8)
 
 for i in range(size[0]):
-    for j in range(size[1]):
+    for j in range(1, size[1]):
         sp = j*9
         img[i][j] = [int(content[i][sp:sp+3]), int(content[i][sp+3:sp+6]), int(content[i][sp+6:sp+9])]
 
