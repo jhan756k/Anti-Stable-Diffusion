e4e89d8d3cd3e44ccdf581c798d8f6ec37afcef8
Jooney <42194130+jhan756k@users.noreply.github.com>
2022-11-19 20:27:47 +0900

    small

--- Image_Viewer/convert.py
+++ Image_Viewer/convert.py
@@ -5,11 +5,11 @@ file = open("./gorani.jh", "w")
 path = os.getcwd()
 img = cv2.imread("{p}/images/original_image/gorani.png".format(p=path))
 
-file.write(str(img.shape[0]) + "," + str(img.shape[1]) + "," + str(img.shape[2]) + "/")
+file.write(str(img.shape[0]).zfill(3) + str(img.shape[1]).zfill(3) + str(img.shape[2]).zfill(3))
 
 for x in range(img.shape[0]):
     for y in range(img.shape[1]):
-        file.write(str(img[x, y][0]) + "," + str(img[x, y][1]) + "," + str(img[x, y][2]) + "/")
-    file.write("ls")
+        file.write(str(img[x, y][0]).zfill(3) + str(img[x, y][1]).zfill(3) + str(img[x, y][2]).zfill(3))
+    file.write(" ")
 
 file.close()
--- Image_Viewer/read.py
+++ Image_Viewer/read.py
@@ -1,12 +1,22 @@
 import numpy as np
+import cv2
 
 file = open("gorani.jh", "r")
 content = file.read()
-filedata = list(content.split("\n"))
+stmp = content[:9]
+size = [int(stmp[:3]), int(stmp[3:6]), int(stmp[6:9])]
+content = content[9:]
+content = list(content.split(" "))
 
-size = list(map(int, filedata[0].split(",")))
-filedata.pop(0)
+img = np.zeros((size[0], size[1], size[2]), dtype=np.uint8)
 
-print(filedata)
+for i in range(size[0]):
+    for j in range(size[1]):
+        sp = j*9
+        img[i][j] = [int(content[i][sp:sp+3]), int(content[i][sp+3:sp+6]), int(content[i][sp+6:sp+9])]
+
+cv2.imshow("Image", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
 file.close()
