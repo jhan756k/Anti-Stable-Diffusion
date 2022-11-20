01d63166d81b8ed508c5ef36bf0db098f8d3998c
Jooney <42194130+jhan756k@users.noreply.github.com>
2022-11-20 15:31:00 +0900

    viewer_fix

--- Image_Viewer/read.py
+++ Image_Viewer/read.py
@@ -13,6 +13,7 @@ for i in range(size[0]-1):
     for j in range(size[1]-1):
         img[i][j] = [int(content[i][sp:sp+3]), int(content[i][sp+3:sp+6]), int(content[i][sp+6:sp+9])]
         sp += 9
+    sp=0
 
 cv2.imshow("Press c to terminate", img)
 cv2.waitKey(0)
