6f3eb82ab6247fe1bc8ef1dbfaaed452497da99b
Jooney <42194130+jhan756k@users.noreply.github.com>
2022-11-24 17:05:24 +0900

    cryptofix

--- Image_Viewer/convert.py
+++ Image_Viewer/convert.py
@@ -1,14 +1,12 @@
-import os, cv2, zlib, time
+import os, cv2, time
 from Crypto.PublicKey import RSA
 from Crypto.Random import get_random_bytes
 from Crypto.Cipher import AES, PKCS1_OAEP
 
-file = open("eagle.jhp", "wb")
+start = time.perf_counter()
 
 path = os.getcwd()
 img = cv2.imread("{p}/images/original_image/forest.png".format(p=path))
-
-start = time.perf_counter()
 
 text = str(img.shape[0]).zfill(4) + str(img.shape[1]).zfill(4) + str(img.shape[2]).zfill(4)
 
@@ -18,17 +16,16 @@ for x in range(img.shape[0]):
 
 key = RSA.generate(2048)
 private_key = key.export_key()
-file_out = open("private.pem", "wb")
-file_out.write(private_key)
-file_out.close()
+file = open("private.pem", "wb")
+file.write(private_key)
+file.close()
 
 public_key = key.publickey().export_key()
-file_out = open("receiver.pem", "wb")
-file_out.write(public_key)
-file_out.close()
+file = open("receiver.pem", "wb")
+file.write(public_key)
+file.close()
 
 data = text.encode("utf-8")
-file_out = open("forest.jhp", "wb")
 recipient_key = RSA.import_key(open("receiver.pem").read())
 session_key = get_random_bytes(16)
 cipher_rsa = PKCS1_OAEP.new(recipient_key)
@@ -36,13 +33,9 @@ enc_session_key = cipher_rsa.encrypt(session_key)
 cipher_aes = AES.new(session_key, AES.MODE_EAX)
 ciphertext, tag = cipher_aes.encrypt_and_digest(data)
 
-# comp_text = zlib.compress(text.encode("utf-8"), 9)
-[ file_out.write(x) for x in (enc_session_key, cipher_aes.nonce, tag, ciphertext) ]
-file_out.close()
-comp_text = zlib.compress(text.encode("utf-8"), 9)
-file.write(comp_text)
+file = open("forest.jhp", "wb")
+[ file.write(x) for x in (enc_session_key, cipher_aes.nonce, tag, ciphertext) ]
 file.close()
 
 end = time.perf_counter()
-
 print("Time taken: {t}".format(t=end-start))
