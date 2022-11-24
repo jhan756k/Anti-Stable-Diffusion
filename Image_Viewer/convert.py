a35103bae3561ff8ad263c3cda6d1848087be2ba
Jooney <42194130+jhan756k@users.noreply.github.com>
2022-11-24 16:37:45 +0900

    RSA

--- Image_Viewer/convert.py
+++ Image_Viewer/convert.py
@@ -1,17 +1,38 @@
 import os, cv2, zlib
-
-file = open("eagle.jhp", "wb")
+from Crypto.PublicKey import RSA
+from Crypto.PublicKey import RSA
+from Crypto.Random import get_random_bytes
+from Crypto.Cipher import AES, PKCS1_OAEP
 
 path = os.getcwd()
-img = cv2.imread("{p}/images/research_image/eagle.jpeg".format(p=path))
+img = cv2.imread("{p}/images/original_image/forest.png".format(p=path))
 
 text = str(img.shape[0]).zfill(4) + str(img.shape[1]).zfill(4) + str(img.shape[2]).zfill(4)
 
 for x in range(img.shape[0]):
     for y in range(img.shape[1]):
         text += (str(img[x, y][0]).zfill(3) + str(img[x, y][1]).zfill(3) + str(img[x, y][2]).zfill(3))
-    text += " "
 
-comp_text = zlib.compress(text.encode("utf-8"), 9)
-file.write(comp_text)
-file.close()
+key = RSA.generate(2048)
+private_key = key.export_key()
+file_out = open("private.pem", "wb")
+file_out.write(private_key)
+file_out.close()
+
+public_key = key.publickey().export_key()
+file_out = open("receiver.pem", "wb")
+file_out.write(public_key)
+file_out.close()
+
+data = text.encode("utf-8")
+file_out = open("forest.jhp", "wb")
+recipient_key = RSA.import_key(open("receiver.pem").read())
+session_key = get_random_bytes(16)
+cipher_rsa = PKCS1_OAEP.new(recipient_key)
+enc_session_key = cipher_rsa.encrypt(session_key)
+cipher_aes = AES.new(session_key, AES.MODE_EAX)
+ciphertext, tag = cipher_aes.encrypt_and_digest(data)
+
+# comp_text = zlib.compress(text.encode("utf-8"), 9)
+[ file_out.write(x) for x in (enc_session_key, cipher_aes.nonce, tag, ciphertext) ]
+file_out.close()
--- Image_Viewer/read.py
+++ Image_Viewer/read.py
@@ -1,22 +1,36 @@
 import numpy as np
 import cv2, zlib
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import AES, PKCS1_OAEP
 
-file = open("eagle.jhp", "rb")
-content = zlib.decompress(file.read()).decode("utf-8")
+file = open("forest.jhp", "rb")
+
+private_key = RSA.import_key(open("private.pem").read())
+
+enc_session_key, nonce, tag, ciphertext = \
+   [ file.read(x) for x in (private_key.size_in_bytes(), 16, 16, -1) ]
+
+file.close()
+
+cipher_rsa = PKCS1_OAEP.new(private_key)
+session_key = cipher_rsa.decrypt(enc_session_key)
+cipher_aes = AES.new(session_key, AES.MODE_EAX, nonce)
+data = cipher_aes.decrypt_and_verify(ciphertext, tag)
+content = data.decode("utf-8")
+
+# file = open("forest.jhp", "rb")
+# content = zlib.decompress(file.read()).decode("utf-8")
+
 stmp = content[:12]
 size = [int(stmp[:4]), int(stmp[4:8]), int(stmp[8:12])]
-content = list(content.split(" "))
 img = np.zeros((size[0], size[1], size[2]), dtype=np.uint8)
 
 sp = 12
-for i in range(size[0]-1):
-    for j in range(size[1]-1):
-        img[i][j] = [int(content[i][sp:sp+3]), int(content[i][sp+3:sp+6]), int(content[i][sp+6:sp+9])]
+for i in range(size[0]):
+    for j in range(size[1]):
+        img[i][j] = [int(content[sp:sp+3]), int(content[sp+3:sp+6]), int(content[sp+6:sp+9])]
         sp += 9
-    sp=0
 
 cv2.imshow("Press c to terminate", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
-file.close()
