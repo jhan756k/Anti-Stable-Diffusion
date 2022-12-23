262f82294ab4edf8ae6d8711a7c2636401aa26e4
Jooney <42194130+jhan756k@users.noreply.github.com>
2022-12-23 18:06:05 +0900

    blur fix

--- AntiCopyBlur/EdgeBlur.py
+++ AntiCopyBlur/EdgeBlur.py
@@ -2,6 +2,7 @@ import cv2, os
 import numpy as np
 from cannyEdge import canny
 
+'''
 kernel2d = np.array([
     [0.9782436, 0.97248424, 0.97034105, 0.97248424, 0.9782436 ],
     [0.97248424, 0.98568037, 0.98314849, 0.98568037, 0.97248424],
@@ -9,8 +10,8 @@ kernel2d = np.array([
     [0.97248424, 0.98568037, 0.98314849, 0.98568037, 0.97248424],
     [0.9782436, 0.97248424, 0.97034105, 0.97248424, 0.9782436 ]
 ])
-
 kernel2d = np.array(kernel2d)
+'''
 
 path = os.getcwd()
 imgname = "forest"
@@ -19,17 +20,27 @@ canny(imgname)
 cannyimg = cv2.imread("{p}/images/canny_image/{ing}_canny.png".format(p=path, ing=imgname), cv2.IMREAD_GRAYSCALE)
 orig_img = cv2.imread("{p}/images/original_image/{ing}.png".format(p=path, ing=imgname))
 
-for x in range(0, cannyimg.shape[0]):
-    for y in range(0, cannyimg.shape[1]):
+for x in range(1, cannyimg.shape[0]-1):
+    for y in range(1, cannyimg.shape[1]-1):
         if cannyimg[x][y] == 255:
             
+            for p in range(3):
+                sum = 0
+
+                for i in range(3):
+                    for j in range(3):
+                        sum += orig_img[x+i-1][y+j-1][p] # 커널 만들어서 여따 곱해도 됨 근데 너무 느림
+                orig_img[x][y][p] = sum // 9
+
+            '''
             for tx in range(5):
                 for ty in range(5):
                     
                     for i in range(3):
                         if 0<=x+tx-2<cannyimg.shape[0] and 0<=y+ty-2<cannyimg.shape[1]:
                             orig_img[x+tx-2][y+ty-2][i] *= kernel2d[tx][ty]
-            
+            '''
+
 cv2.imwrite("{p}/images/edgeblur_image/{ing}_edgeblur.png".format(p=path, ing=imgname), orig_img)
 
 '''
