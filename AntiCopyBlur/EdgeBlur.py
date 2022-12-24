f20da6ad227fbbf7bb3e7bf943c705d6eee07f0c
Jooney <42194130+jhan756k@users.noreply.github.com>
2022-12-24 13:02:18 +0900

    major blur fix

--- AntiCopyBlur/EdgeBlur.py
+++ AntiCopyBlur/EdgeBlur.py
@@ -1,5 +1,5 @@
 import cv2, os
-import numpy as np
+from skimage.metrics import structural_similarity as ssim, peak_signal_noise_ratio as psnr
 from cannyEdge import canny
 
 '''
@@ -17,22 +17,28 @@ path = os.getcwd()
 imgname = "eagle"
 canny(imgname)
 
-cannyimg = cv2.imread("{p}/images/canny_image/{ing}_canny.png".format(p=path, ing=imgname), cv2.IMREAD_GRAYSCALE)
+cannyimg = cv2.imread("{p}/images/canny_image/{ing}_canny.png".format(p=path, ing=imgname), cv2.IMREAD_GRAYSCALE)   
 orig_img = cv2.imread("{p}/images/original_image/{ing}.png".format(p=path, ing=imgname))
 
-n = 30 # 커널 크기
+kernelSize = 5 # 블러 커널 크기 
+nearbyBlurSize = 5 # 블러 처리할 주변 픽셀 수
 
-for x in range(n//2, cannyimg.shape[0]-(n//2)):
-    for y in range(n//2, cannyimg.shape[1]-(n//2)):
-        if cannyimg[x][y] == 255:
-            
-            for p in range(3):
-                sum = 0
+term = (kernelSize//2) + (nearbyBlurSize//2)
 
-                for i in range(n):
-                    for j in range(n):
-                        sum += orig_img[x+i-(n//2)][y+j-(n//2)][p] # 커널 만들어서 여따 곱해도 됨 근데 너무 느림
-                orig_img[x][y][p] = sum // (n*n)
+for x in range(term, cannyimg.shape[0]-term):
+        for y in range(term, cannyimg.shape[1]-term):
+            if cannyimg[x][y] == 255:
+                
+                for t in range(x-(nearbyBlurSize//2), x+(nearbyBlurSize//2)):
+                    for c in range(y-(nearbyBlurSize//2), y+(nearbyBlurSize//2)):
+                            
+                        for p in range(3):
+                            sum = 0
+
+                            for i in range(kernelSize):
+                                for j in range(kernelSize):
+                                    sum += orig_img[t+i-term][c+j-term][p] 
+                            orig_img[t][c][p] = sum // (kernelSize*kernelSize)
 
             '''
             for tx in range(5):
@@ -44,6 +50,7 @@ for x in range(n//2, cannyimg.shape[0]-(n//2)):
             '''
 
 cv2.imwrite("{p}/images/edgeblur_image/{ing}_edgeblur.png".format(p=path, ing=imgname), orig_img)
+print(imgname, psnr(cv2.imread("{p}/images/original_image/{ing}.png".format(p=path, ing=imgname)), cv2.imread("{p}/images/edgeblur_image/{ing}_edgeblur.png".format(p=path, ing=imgname))), ssim(cv2.imread("{p}/images/original_image/{ing}.png".format(p=path, ing=imgname)), cv2.imread("{p}/images/edgeblur_image/{ing}_edgeblur.png".format(p=path, ing=imgname)), multichannel=True))
 
 '''
 dx = [1, 0, -1, 0]
