95aba459d921fccaf264ff1d7816dee5143f0d86
Jooney <42194130+jhan756k@users.noreply.github.com>
2022-12-24 20:32:25 +0900

    time

--- AntiCopyBlur/EdgeBlur.py
+++ AntiCopyBlur/EdgeBlur.py
@@ -1,4 +1,4 @@
-import cv2, os
+import cv2, os, time
 import numpy as np
 from skimage.metrics import structural_similarity as ssim, peak_signal_noise_ratio as psnr
 from cannyEdge import canny
@@ -9,8 +9,13 @@ kernel2d = np.outer(kernel1d, kernel1d.transpose())
 '''
 
 path = os.getcwd()
-imgname = "eagle"
+imgname = "pelican"
+bc = time.perf_counter()
 canny(imgname)
+ac = time.perf_counter()
+print("Canny Edge Detection Time: ", ac-bc)
+
+sb = time.perf_counter()
 
 cannyimg = cv2.imread("{p}/images/canny_image/{ing}_canny.png".format(p=path, ing=imgname), cv2.IMREAD_GRAYSCALE)   
 orig_img = cv2.imread("{p}/images/original_image/{ing}.png".format(p=path, ing=imgname))
@@ -19,11 +24,12 @@ kernelSize = 3 # 블러 커널 크기
 nearbyBlurSize = 3 # 블러 처리할 주변 픽셀 수
 
 term = (kernelSize//2) + (nearbyBlurSize//2)
+canny_number = 0
 
 for x in range(term, cannyimg.shape[0]-term):
         for y in range(term, cannyimg.shape[1]-term):
             if cannyimg[x][y] == 255:
-                
+                canny_number += 1
                 for t in range(x-(nearbyBlurSize//2), x+(nearbyBlurSize//2)):
                     for c in range(y-(nearbyBlurSize//2), y+(nearbyBlurSize//2)):
                             
@@ -36,17 +42,14 @@ for x in range(term, cannyimg.shape[0]-term):
 
                             orig_img[t][c][p] = sum // (kernelSize*kernelSize)
 
-            '''
-            for tx in range(5):
-                for ty in range(5):
-                    
-                    for i in range(3):
-                        if 0<=x+tx-2<cannyimg.shape[0] and 0<=y+ty-2<cannyimg.shape[1]:
-                            orig_img[x+tx-2][y+ty-2][i] *= kernel2d[tx][ty]
-            '''
+eb = time.perf_counter()
 
-'''
+print("Edge Blur Time: ", eb-sb)
+print("Canny Number: ", canny_number)
+
+
 cv2.imwrite("{p}/images/edgeblur_image/{ing}_edgeblur.png".format(p=path, ing=imgname), orig_img)
+'''
 print(imgname, psnr(cv2.imread("{p}/images/original_image/{ing}.png".format(p=path, ing=imgname)), cv2.imread("{p}/images/edgeblur_image/{ing}_edgeblur.png".format(p=path, ing=imgname))), ssim(cv2.imread("{p}/images/original_image/{ing}.png".format(p=path, ing=imgname)), cv2.imread("{p}/images/edgeblur_image/{ing}_edgeblur.png".format(p=path, ing=imgname)), multichannel=True))
 '''
 
--- AntiCopyBlur/cannyEdge.py
+++ AntiCopyBlur/cannyEdge.py
@@ -6,11 +6,10 @@ path = os.getcwd()
 def canny(imgname):
     img = cv2.imread("{p}/images/original_image/{ing}.png".format(p=path, ing=imgname), cv2.IMREAD_GRAYSCALE)
 
-    sigma = 0.33
+    sigma = 0.33 
     v = np.median(img)
     min_threshold = int(max(0, (1.0 - sigma) * v))
     max_threshold = int(min(255, (1.0 + sigma) * v))
-
 
     '''
     min_threshold = 255/3
